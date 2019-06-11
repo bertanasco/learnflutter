c533f75e9bc27c9c67259bbddb63ce47c425f6ec
Bert <banasco@4impact.com.au>
2019-06-11 12:55:53 +0800

    Added code for Lesson 1.13 Custom widget

---
+++ Lesson1_13/lesson_1_13_category_widget/android/app/src/main/kotlin/com/banasco/lesson_1_13_category_widget/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.banasco.lesson_1_13_category_widget
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
