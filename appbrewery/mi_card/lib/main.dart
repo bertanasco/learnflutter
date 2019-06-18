9a4a4d39a5e70d9efdc2aad201eba1c059b7a3bf
banasco <bertanasco@gmail.com>
2019-06-18 09:17:18 +0800

    added own version of miCard withour usinng cards and listtile

--- appbrewery/mi_card/lib/main.dart
+++ appbrewery/mi_card/lib/main.dart
@@ -12,6 +12,7 @@ class App extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
@@ -23,6 +24,64 @@ class App extends StatelessWidget {
                   fontSize: 40,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico",
+                ),
+              ),
+              Text(
+                "Software Engineer",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  //fontWeight: FontWeight.bold,
+                  fontFamily: "SourceSansPro",
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 10.0,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "+ 63 917 59247576",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 10.0,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "bertanasco@gmail.com",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.center,
                 ),
               ),
             ],
